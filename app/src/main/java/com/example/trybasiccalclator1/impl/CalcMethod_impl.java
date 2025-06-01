d6397b59921a683ca7b1f1e1c3a17a35c86fd588
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-06-01 15:55:28 +0200

    commited

--- app/src/main/java/com/example/trybasiccalclator1/impl/CalcMethod_impl.java
+++ app/src/main/java/com/example/trybasiccalclator1/impl/CalcMethod_impl.java
@@ -5,15 +5,20 @@ import com.example.trybasiccalclator1.CalcMethod;
 public class CalcMethod_impl implements CalcMethod {
     private double currentNumber;
     private double storedNumber;
+    private String operator;
 
     public CalcMethod_impl() {
         clear();
     }
 
-
     @Override
     public void clear() {
         this.storedNumber = 0;
         this.currentNumber = 0;
+        this.operator = "";
     }
+
+    private void calcResult(){}
+
+    private void applyOperator(){}
 }
