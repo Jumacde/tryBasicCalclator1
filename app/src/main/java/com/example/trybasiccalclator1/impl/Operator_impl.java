cd579ac0d1aa548b44cfa8494fc73a212f26b5de
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-06-06 19:08:47 +0200

    commited

--- app/src/main/java/com/example/trybasiccalclator1/impl/Operator_impl.java
+++ app/src/main/java/com/example/trybasiccalclator1/impl/Operator_impl.java
@@ -42,7 +42,24 @@ public class Operator_impl implements Operator {
      * to avoid cross-reference issues
      * **/
     private void processOperator(CalcLogic calcLogic, String operator){
-
+        double x = calcLogic.getCurrentNumber();
+        // if a operator between the current and stored number already is and no number is after that inputted.
+        // ex. 1 + 1
+        if (!currentOperator.isEmpty() && !calcLogic.getIsInputNum()) {
+            calcLogic.callCalcResult(operator); // calculate by the current operator
+            calcLogic.setStoredNumber(x); // store the calc result for the next calculate
+            calcLogic.setIsInputNum(true); // allow to input the next number
+            setCurrentOperator(operator); // set a new operator
+        } else if (operator.equals("=")) { // ex. 1 + 1 =
+            calcLogic.callCalcResult(currentOperator); // calculate
+            setCurrentOperator(""); // clear the operator after "="
+            calcLogic.setStoredNumber(calcLogic.getCurrentNumber()); // save the calculate result
+            calcLogic.setIsInputNum(true);
+        } else { // ex. you will input a operator after 1 + 1
+            setCurrentOperator(operator); // ex. 1 + 1 *
+            calcLogic.setStoredNumber(calcLogic.getCurrentNumber()); // save the current numbers as stored numebr. ex. 1 + 1
+            calcLogic.setIsInputNum(true); // allow input a new number after stored number. ex. 1 + 1 * 2
+        }
     }
 
 
