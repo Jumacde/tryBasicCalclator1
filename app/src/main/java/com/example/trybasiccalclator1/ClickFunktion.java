58a33e61c90b084802e512e89e81d5aa08d1f510
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-06-07 12:58:14 +0200

    commited

--- app/src/main/java/com/example/trybasiccalclator1/ClickFunktion.java
+++ app/src/main/java/com/example/trybasiccalclator1/ClickFunktion.java
@@ -1,7 +1,7 @@
 package com.example.trybasiccalclator1;
 
 public interface ClickFunktion {
-
+    // wrap methods
     void callDigitClick(CalcLogic calcLogic, Display display, Operator operatorProcessor, int digit);
     void callACClick(CalcLogic calcLogic, Display display, Operator operatorProcessor);
     void callOperatorClick(CalcLogic calcLogic, Display display, Operator operatorProcessor, String operator);
--- app/src/main/java/com/example/trybasiccalclator1/impl/ClickFunktion_impl.java
+++ app/src/main/java/com/example/trybasiccalclator1/impl/ClickFunktion_impl.java
@@ -7,22 +7,51 @@ import com.example.trybasiccalclator1.Operator;
 
 public class ClickFunktion_impl implements ClickFunktion {
 
-    public ClickFunktion_impl() {}
+    private Operator callProcessOperator;
 
+    public ClickFunktion_impl(Operator callProcessOperator) {
+        this.callProcessOperator = callProcessOperator;
+    }
 
+    /**
+     * - Wrapper methods
+     * allow to access other classes private method(s)
+     * @ Param: calcLogic, display
+     * call interfaces
+     * @ Param: operatorProcessor
+     * call this method from the interface Operator
+     * @ Param: digit
+     * **/
     @Override
     public void callDigitClick(CalcLogic calcLogic, Display display, Operator operatorProcessor, int digit) {
-
+        digitClick(calcLogic, display, operatorProcessor, digit);
     }
 
+    /**
+     * - Wrapper methods
+     * allow to access other classes private method(s)
+     * @ Param: calcLogic, display
+     * call interfaces
+     * @ Param: operatorProcessor
+     * call this method from the interface Operator
+     * **/
     @Override
     public void callACClick(CalcLogic calcLogic, Display display, Operator operatorProcessor) {
-
+        aCClick(calcLogic, display, operatorProcessor);
     }
 
+    /**
+     * - Wrapper methods
+     * allow to access other classes private method(s)
+     * @ Param: calcLogic, display
+     * call interfaces
+     * @ Param: operatorProcessor
+     * call this method from the interface Operator
+     * @ Param: operator
+     * **/
     @Override
     public void callOperatorClick(CalcLogic calcLogic, Display display, Operator operatorProcessor, String operator) {
-
+        operatorClick(calcLogic, display, operatorProcessor, operator);
     }
 
     private void digitClick(CalcLogic calcLogic, Display display, Operator operatorProcessor, int digit){
